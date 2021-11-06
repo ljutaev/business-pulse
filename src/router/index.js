d1723603f5469ddf9ff8ee7b05792187538ace0e
ljutaev <ljutaev@gmail.com>
2021-11-06 13:59:19 +0200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NotFound from "../views/NotFound.vue";
+import Review from "../views/Review.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Review",
+    meta: {
+      title: "Review",
+    },
+    component: Review,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
+    path: "*",
+    name: NotFound,
+    meta: {
+      title: "404",
+    },
+    component: () => import("../views/NotFound.vue"),
+  },
+];
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+  mode: "history",
+  routes,
+  scrollBehavior(savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
+});
 
-export default router
+router.afterEach((to) => {
+  if (to.meta && to.meta.title) {
+    document.title =
+      to.meta.title + " | Business Pulse";
+  }
+});
+
+export default router;
---
+++ src/views/Review.vue
@@ -0,0 +1,68 @@
+<template>
+	<div>
+		<div class="container">	
+			<div class="review-wrap">
+				<div class="review-card">
+					<div class="review-card__header">
+						
+					</div>
+					<div class="review-card__content">
+						<div class="review-card__heading">Загальне враження*</div>
+						<div class="review-card__stars">
+							<ul class="rating-card__list">
+								<li class="rating-card__list__item">
+									<label>
+										<svg class="star-ico" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+											<path _ngcontent-c16="" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+											<linearGradient id="StarGradient0"><stop offset="0%" stop-color="#FC8885"></stop><stop offset="100%" stop-color="#FB7C7F"></stop></linearGradient>
+										</svg>
+										<div>Жахливо</div>
+									</label>
+								</li>
+								<li class="rating-card__list__item">
+									<label><svg class="star-ico" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+											<path _ngcontent-c16="" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+											<linearGradient _ngcontent-c16="" id="StarGradient0"><stop offset="0%" stop-color="#FC8885"></stop><stop offset="100%" stop-color="#FB7C7F"></stop></linearGradient>
+										</svg><div>Погано</div></label>
+								</li>
+								<li class="rating-card__list__item">
+									<label><svg class="star-ico" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+											<path _ngcontent-c16="" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+											<linearGradient _ngcontent-c16="" id="StarGradient0"><stop offset="0%" stop-color="#FC8885"></stop><stop offset="100%" stop-color="#FB7C7F"></stop></linearGradient>
+										</svg><div>Добре</div></label>
+								</li>
+								<li class="rating-card__list__item">
+									<label><svg class="star-ico" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+											<path _ngcontent-c16="" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+											<linearGradient id="StarGradient0"><stop offset="0%" stop-color="#FC8885"></stop><stop offset="100%" stop-color="#FB7C7F"></stop></linearGradient>
+										</svg><div>Відмінно</div></label>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="review-socials">
+					<a href="#">fb</a>
+					<a href="#">g</a>
+					<a href="#">ins</a>
+					<a href="#">twit</a>
+				</div>
+			</div>
+		</div>	
+	</div>	
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="sass" scoped>
+	.container
+		max-width: 576px
+		margin: 0 auto
+		padding: 0 15px
+
+
+</style>
